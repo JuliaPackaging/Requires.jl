91eba09473eac916807ae9171761c5147f16f292
Tim Holy <tim.holy@gmail.com>
2019-11-24 03:54:49 -0600

    Add some `precompile` statements
    These only take a couple of milliseconds to infer, but we might as well 
    cache the inference result.

--- src/Requires.jl
+++ src/Requires.jl
@@ -9,4 +9,12 @@ function __init__()
     push!(package_callbacks, loadpkg)
 end
 
+if isprecompiling()
+    @assert precompile(loadpkg, (Base.PkgId,))
+    @assert precompile(withpath, (Any, String))
+    @assert precompile(err, (Any, Module, String))
+    @assert precompile(parsepkg, (Expr,))
+    @assert precompile(listenpkg, (Any, Base.PkgId))
+end
+
 end # module
